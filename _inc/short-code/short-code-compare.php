a2ce144ef17b3a10e76337ef6e08ebbe051bf701
mahdi <mdavoudi47@gmail.com>
2026-02-10 01:14:31 +0330

    Update short-code-compare.php

--- _inc/short-code/short-code-compare.php
+++ _inc/short-code/short-code-compare.php
@@ -16,6 +16,9 @@ function tns_compare_func() : string {
     $rows = $wpdb->get_results(
         $wpdb->prepare("SELECT * FROM {$table} WHERE u_id = %d", $user_id)
     );
+    $products = wc_get_products($rows);
+    $counter = 1;
+
 
     ob_start(); ?>
 
@@ -40,162 +43,101 @@ function tns_compare_func() : string {
 
         <div class="page-content">
             <div class="container">
+                <?php if (!empty($rows)) : ?>
+                <table class="table table-wishlist table-mobile text-center">
+                    <thead>
+                    <tr>
+                        <th>ردیف</th>
+                        <th>نام محصول</th>
+                        <th>تصویر محصول</th>
+                        <th>قیمت</th>
+                        <th>وضعیت محصول</th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                    </thead>
 
-                <div class="compare-table table-responsive">
-                    <table class="table mb-0">
+                    <tbody>
 
-                        <?php if ($rows) : ?>
-                            <tbody>
+                        <?php foreach ($rows as $item) : ?>
+                        <?php foreach ($products as $product) : ?>
+                    <tr>
+                <td class="price-col">
+                    <div>
+                        <h3 class="product-title ">
+                            <?php echo $counter++; ?>
+                        </h3><!-- End .product-title -->
+                    </div><!-- End .product -->
+                </td>
+                <td class="price-col">
+                            <div>
+                                <h3 class="product-title text-center">
+                                    <a href="<?php echo get_the_permalink($item->p_id); ?>"><?php echo esc_html($item->p_title); ?></a>
+                                </h3><!-- End .product-title -->
+                            </div><!-- End .product -->
+                        </td>
+                <td class="price-col">
+                    <div class="product">
+                        <figure class="product-media">
+                            <a href="<?php echo get_the_permalink($item->p_id); ?>">
+                                <img src="<?php echo esc_url(get_the_post_thumbnail_url($item->p_id)); ?>" alt="<?php echo esc_attr($item->p_id); ?>">
+                            </a>
+                        </figure>
+                    </div><!-- End .product -->
+                </td>
 
-                            <!-- محصولات -->
-                            <tr>
-                                <td class="first-column">محصول</td>
+                <td class="price-col"><?php echo $product->get_price();  ?>  تومان </td>
+                <td class="stock-col"><span class="in-stock"><?php echo  $product->is_in_stock() ? 'موجود' : 'ناموجود' ?></span></td>
+                <td class="action-col">
+                    <a  href="<?php echo esc_url($product->add_to_cart_url()); ?>" class="btn btn-block btn-outline-primary-2"><i
+                                class="icon-cart-plus"></i>افزودن به سبد خرید</a>
+                </td>
+                <td class="remove-col text-left bookmark-product-compare" data-pid="<?php echo $item->p_id ?>"><button class="btn-remove"><i
+                                class="icon-close"></i></button></td>
+            </tr>
+        <?php endforeach;?>
+        <?php endforeach; ?>
 
-                                <?php foreach ($rows as $item):
-                                    $product = wc_get_product($item->p_id);
-                                    if(!$product) continue;
-                                    ?>
+                    </tbody>
 
-                                    <td class="product-image-title">
+                </table><!-- End .table table-wishlist -->
+                <?php else : ?>
+                    <div class="alert alert-info text-center">محصولی برای مقایسه وجود ندارد!</div>
+                <?php endif; ?>
+            <div class="wishlist-share mt-4">
+                <div class="social-icons social-icons-sm">
 
-                                        <a href="<?php echo get_permalink($product->get_id()); ?>" class="image">
-                                            <?php echo $product->get_image('woocommerce_thumbnail'); ?>
-                                        </a>
+                    <label class="social-label">اشتراک گذاری در:</label>
 
-                                        <a href="<?php echo get_permalink($product->get_id()); ?>" class="title">
-                                            <?php echo $product->get_title(); ?>
-                                        </a>
+                    <a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo urlencode(get_permalink()); ?>"
+                       class="social-icon" target="_blank">
+                        <i class="icon-facebook-f"></i>
+                    </a>
 
-                                    </td>
+                    <a href="https://twitter.com/intent/tweet?url=<?php echo urlencode(get_permalink()); ?>"
+                       class="social-icon" target="_blank">
+                        <i class="icon-twitter"></i>
+                    </a>
 
-                                <?php endforeach; ?>
+                    <a href="https://t.me/share/url?url=<?php echo urlencode(get_permalink()); ?>"
+                       class="social-icon" target="_blank">
+                        <i class="icon-telegram"></i>
+                    </a>
 
-                            </tr>
+                    <a href="https://wa.me/?text=<?php echo urlencode(get_permalink()); ?>"
+                       class="social-icon" target="_blank">
+                        <i class="icon-whatsapp"></i>
+                    </a>
 
-                            <!-- توضیحات -->
-                            <tr>
-                                <td class="first-column">توضیحات</td>
+                    <a href="https://www.instagram.com/"
+                       class="social-icon" target="_blank">
+                        <i class="icon-instagram"></i>
+                    </a>
 
-                                <?php foreach ($rows as $item):
-                                    $product = wc_get_product($item->p_id);
-                                    if(!$product) continue; ?>
-                                    <td class="pro-desc">
-                                        <p><?php echo wp_trim_words($product->get_short_description(), 20); ?></p>
-                                    </td>
-                                <?php endforeach; ?>
-
-                            </tr>
-
-                            <!-- قیمت -->
-                            <tr>
-                                <td class="first-column">قیمت</td>
-
-                                <?php foreach ($rows as $item):
-                                    $product = wc_get_product($item->p_id); ?>
-                                    <td class="pro-price">
-                                        <?php echo $product->get_price_html(); ?>
-                                    </td>
-                                <?php endforeach; ?>
-
-                            </tr>
-                            <tr>
-                                <td class="first-column">افزودن به سبد خرید</td>
-                                <td class="pro-addtocart"><button src="<?php echo esc_url($item->add_to_cart_url()); ?>" class="btn btn-block btn-outline-primary-2"><i
-                                                class="icon-cart-plus"></i>افزودن به سبد خرید</button></td>
-                            </tr>
-
-                            <!-- موجودی -->
-                            <tr>
-                                <td class="first-column">موجودی</td>
-
-                                <?php foreach ($rows as $item):
-                                    $product = wc_get_product($item->p_id); ?>
-                                    <td class="pro-stock">
-                                        <?php if ($product->is_in_stock()): ?>
-                                            <span class="in-stock">موجود</span>
-                                        <?php else: ?>
-                                            <span class="out-of-stock">ناموجود</span>
-                                        <?php endif; ?>
-                                    </td>
-                                <?php endforeach; ?>
-                            </tr>
-
-                            <!-- حذف -->
-                            <tr>
-                                <td class="first-column">حذف</td>
-
-                                <?php foreach ($rows as $item): ?>
-                                    <td class="pro-remove">
-                                        <a href="?remove_compare=<?php echo $item->id; ?>"
-                                           class="btn btn-block btn-outline-danger">
-                                            <i class="icon-close"></i> حذف
-                                        </a>
-                                    </td>
-                                <?php endforeach; ?>
-
-
-                            </tr>
-                            <tr>
-                                <td class="first-column">امتیاز</td>
-                                <td class="pro-ratting">
-                                    <i class="icon-star-o"></i>
-                                    <i class="icon-star"></i>
-                                    <i class="icon-star"></i>
-                                    <i class="icon-star"></i>
-                                    <i class="icon-star"></i>
-                                </td>
-                            </tbody>
-
-                        <?php else : ?>
-                            <div class="alert alert-info text-center">
-                                محصولی برای مقایسه وجود ندارد!
-                            </div>
-                        <?php endif; ?>
-
-                    </table>
                 </div>
-
-                <!-- اشتراک‌گذاری کامل -->
-                <div class="wishlist-share mt-4">
-                    <div class="social-icons social-icons-sm">
-
-                        <label class="social-label">اشتراک گذاری در:</label>
-
-                        <a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo urlencode(get_permalink()); ?>"
-                           class="social-icon" target="_blank">
-                            <i class="icon-facebook-f"></i>
-                        </a>
-
-                        <a href="https://twitter.com/intent/tweet?url=<?php echo urlencode(get_permalink()); ?>"
-                           class="social-icon" target="_blank">
-                            <i class="icon-twitter"></i>
-                        </a>
-
-                        <a href="https://t.me/share/url?url=<?php echo urlencode(get_permalink()); ?>"
-                           class="social-icon" target="_blank">
-                            <i class="icon-telegram"></i>
-                        </a>
-
-                        <a href="https://wa.me/?text=<?php echo urlencode(get_permalink()); ?>"
-                           class="social-icon" target="_blank">
-                            <i class="icon-whatsapp"></i>
-                        </a>
-
-                        <a href="https://www.instagram.com/"
-                           class="social-icon" target="_blank">
-                            <i class="icon-instagram"></i>
-                        </a>
-
-                        <a href="<?php echo get_permalink(); ?>" class="social-icon"
-                           onclick="navigator.clipboard.writeText('<?php echo get_permalink(); ?>'); alert('لینک کپی شد!'); return false;">
-                            <i class="icon-link"></i>
-                        </a>
-
-                    </div>
-                </div>
-
             </div>
-        </div>
+            </div><!-- End .container -->
+        </div><!-- End .page-content -->
 
     </main>
 
